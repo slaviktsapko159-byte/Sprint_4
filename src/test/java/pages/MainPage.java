cb6c0b29979fba4c01ac78545fa00623009b6bd5
slaviktsapko159-byte <slaviktsapko159@gmail.com>
2026-03-23 22:14:37 +0500

    азрешение конфликтов

--- src/test/java/pages/MainPage.java
+++ src/test/java/pages/MainPage.java
@@ -9,10 +9,6 @@ public class MainPage {
 
     private final By topOrderButton = By.xpath("//button[text()='Заказать']");
     private final By bottomOrderButton = By.xpath("//button[text()='Заказать']/ancestor::div[contains(@class, 'Home_FinishButton')]//button");
-<<<<<<< HEAD
-=======
-    private final By cookieBanner = By.className("App_CookieConsent__1yUIN");
->>>>>>> 65cad67c99c225c12f6962f4db4e393045808817
     private final By accordionButtons = By.cssSelector(".accordion__button");
 
     private String answerByIndex(int index) {
@@ -21,29 +17,9 @@ public class MainPage {
 
     public MainPage(WebDriver driver) {
         this.driver = driver;
-<<<<<<< HEAD
     }
 
     private void clickElement(By locator) {
-=======
-        removeCookieBanner();
-    }
-
-    private void removeCookieBanner() {
-        try {
-            if (driver.findElements(cookieBanner).size() > 0) {
-                ((JavascriptExecutor) driver).executeScript(
-                        "var banner = document.querySelector('.App_CookieConsent__1yUIN'); if(banner) banner.remove();");
-                new WebDriverWait(driver, 5).until(ExpectedConditions.invisibilityOfElementLocated(cookieBanner));
-            }
-        } catch (Exception e) {
-            // ignore
-        }
-    }
-
-    private void clickElement(By locator) {
-        removeCookieBanner();
->>>>>>> 65cad67c99c225c12f6962f4db4e393045808817
         WebElement element = new WebDriverWait(driver, 10).until(ExpectedConditions.elementToBeClickable(locator));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", element);
@@ -58,10 +34,6 @@ public class MainPage {
     }
 
     public void clickQuestion(int index) {
-<<<<<<< HEAD
-=======
-        removeCookieBanner();
->>>>>>> 65cad67c99c225c12f6962f4db4e393045808817
         WebElement question = driver.findElements(accordionButtons).get(index);
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", question);
         ((JavascriptExecutor) driver).executeScript("arguments[0].click();", question);
